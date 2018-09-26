448f4be0b9eee80e929df95be0bec481a5085e39
Colin Kennedy <colinvfx@gmail.com>
2018-09-26 16:36:41 -0400

    app_launch.py - Added missing documentation

--- hooks/app_launch.py
+++ hooks/app_launch.py
@@ -62,28 +62,52 @@ class AppLaunch(tank.Hook):
 
 class BaseAdapter(object):
 
+    '''An adapter that abstracts the different OS-specific syntaxes for running commands.
+
+    Attributes:
+        shell_type (str): The shell which Rez will use to call the "main" command.
+
+    '''
+
     shell_type = 'bash'
-    # TODO : This should be define-able in a YAML file. There's no reason why it should exist here
-    engines = {
-        'tk-nuke': ('nuke', ),
-    }
 
     @staticmethod
     def get_command(path, args):
-        # Note: Execute the command in the background
-        return '{path} {args} &'.format(path=path, args=args)
+        '''Create a command to run the given path, for Linux.
 
-    @classmethod
-    def get_packages(cls, name):
-        return cls.engines.get(name, tuple())
+        Note:
+            This function runs in the user's background.
+
+        Args:
+            path (str): The full or relative path to the executable to make into a command.
+            args (str): Every argument to add to the generated command.
+
+        Returns:
+            str: The generated command.
+
+        '''
+        # Note: Execute the command in the background
+        return '"{path}" {args} &'.format(path=path, args=args)
 
     @staticmethod
     def get_rez_root_command():
+        '''str: Print the location where rez is installed (if it is installed).'''
         return 'rez-env rez -- printenv REZ_REZ_ROOT'
 
     @classmethod
     def execute(cls, context, args):
-        command = 'Nuke11.2'
+        '''Run the context's main command with the given `args`.
+
+        Args:
+            context (`rez.resolved_context.ResolvedContext`): The context to run "main".
+            args (str): Every argument to add to the generated command.
+
+        Returns:
+            dict[str, str or int]: The results of the command's execution.
+
+        '''
+        # Note: For the package to be valid, it must expose a "main" command.
+        command = 'main'
 
         if args:
             command += ' {args}'.format(args=args)
@@ -105,17 +129,39 @@ class BaseAdapter(object):
         }
 
 
-
 class LinuxAdapter(BaseAdapter):
+
+    '''An adapter that abstracts Linux commands and Rez.'''
+
     pass
 
 
 class WindowsAdapter(BaseAdapter):
 
+    '''An adapter that abstracts Linux commands and Rez.
+
+    Attributes:
+        shell_type (str): The shell which Rez will use to call the "main" command.
+
+    '''
+
     shell_type = 'cmd'
 
     @staticmethod
     def get_command(path, args):
+        '''Create a command to run the given path, for Linux.
+
+        Note:
+            This function runs in the user's background.
+
+        Args:
+            path (str): The full or relative path to the executable to make into a command.
+            args (str): Every argument to add to the generated command.
+
+        Returns:
+            str: The generated command.
+
+        '''
         # Note: We use the "start" command to avoid any command shells from
         # popping up as part of the application launch process.
         #
@@ -123,10 +169,25 @@ class WindowsAdapter(BaseAdapter):
 
     @staticmethod
     def get_rez_root_command():
+        '''str: Print the location where rez is installed (if it is installed).'''
         return 'rez-env rez -- echo %REZ_REZ_ROOT%'
 
 
 def get_adapter(system=''):
+    '''Get an adapter for the given OS.
+
+    Args:
+        system (`str`, optional):
+            The OS to use. If no OS is given, the user's current OS is used, instead.
+            Default: "".
+
+    Raises:
+        NotImplementedError: If the given `system` has no adapter.
+
+    Returns:
+        `BaseAdapter`: The found adapter, if any.
+
+    '''
     if not system:
         system = platform.system()
 
@@ -138,4 +199,5 @@ def get_adapter(system=''):
     try:
         return options[system]
     except KeyError:
-        raise EnvironmentError('system "{system}" is currently unsupported.'.format(system=system))
+        raise NotImplementedError('system "{system}" is currently unsupported. Options were, "{options}"'
+                                  ''.format(system=system, options=list(options)))
